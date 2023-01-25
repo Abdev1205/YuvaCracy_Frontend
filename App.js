3ae2d74901bdf39d831e136e27a6ab46d0440151
ttushar2002 <101193320+ttushar2002@users.noreply.github.com>
2023-01-25 17:24:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+
+  return (
+
+    <header className="App-header">
+
+      <div className="App"><h1>About Us</h1></div>
+      <div className="App3"><h3>Yuvacracy act as a forum for young people to network and socialize!!</h3></div>
+      <div className="App4"><h1> Educate. Engage. Empower</h1></div>
+      <header className='header'>
+
+
+
+
+
+        <div className="App2">
+
+          <div className="head"><h2>Our Mission</h2></div>
+          <h5>Our mission is to empower society though youth enagagements.</h5>
+
+          <p>We aim to do this by galvanizing  young leaders as well as bridging the gap between the youth and experts.
+
+          </p>
+        </div>
+
+
+
+        <div className="App1">
+          <div className="head"><h2>Our Vision</h2></div>
+          <h5>Our vision is to drive the evolution of youth to meet today's challenges.</h5>
+          <p>The youth of a nation shapes and molds its presentand paves the way for its future.</p>
+
+
+        </div>
+      </header>
+
+
+    </header>
+
+
+  );
+}
+
+
+
+export default App;
