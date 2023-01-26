9e2bb23dd13b54a7bd45cf6fdde7936148b2cc12
Sufyan Yahya Isah <safiyanuyahaya0@gmail.com>
2023-01-26 13:16:56 +0100

    Fixed Event Page with its mobile responsiveness

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -10,72 +10,23 @@ export class Events extends PureComponent {
                 <NavBar />
                 <br /><br /><br /><br /><br /><br />
 
-                    <div className="container-main">
-      <div className="container-main-upper">
-        <div className="container-upper-content1">
-          <h1>YUVACRACY</h1>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat,
-            voluptatibus eos! Laborum similique modi, adipisci cupiditate esse
-            saepe nostrum expedita earum pariatur quis iste autem harum culpa
-            accusamus! Qui, aspernatur.
-          </p>
-        </div>
-        <div className="container-upper-content2">
-          <h3>Date & Time</h3>
-          <p>Thursay, Jan 12, 2023 at 20: 30 PM</p>
-          <button className = "button" id="colored-button">Book Now (Free)</button>
-          <button className = "button" id="outline-button">Promoter Program</button>
-        </div>
+                <div className="event-container">
+      <div className="event-container-content-1">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/002/214/132/non_2x/teamwork-creative-advertising-free-vector.jpg"
+          alt="img"
+        />
       </div>
-
-      <div className="container-main-lower">
-        <div className="countdown-container col-lg-6">
-          <h1 id="event-tag">EVENT STARTING IN</h1>
-          <Clock duration={12 * 24 * 60 * 60 * 1000} />
-        </div>
-        <div className="container-lower-content">
-          <h1>Upcoming Events</h1>
-          <div className="item">
-            <div className="img-div">
-              <img
-                src="https://images.unsplash.com/photo-1523580494863-6f3031224c94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt=""
-              />
-            </div>
-            <div className="info-div">
-              <h3>Lorem ipsum dolor sit amet.</h3>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                excepturi, qui perferendis laudantium architecto ea explicabo
-                nam ratione dolorem, aperiam possimus minus voluptates
-                exercitationem nihil aliquid temporibus. Dolor ex, consectetur
-                itaque cumque soluta est eos libero nulla totam ullam natus.
-              </p>
-              <span>29th January 2023 at 20:30 pm</span>
-            </div>
+      <div className="event-container-content-2">
+        <h1>New Events are Coming Soon</h1>
+        <h3>Stay Tuned...</h3>
+        {/* <p>To get Latest Updates about events please subscribe here</p>
+        <form>
+          <div className="form-container">
+            <input type="text" placeholder="Please enter your email address" />
+            <button type="submit">Subcribe here</button>
           </div>
-          <hr />
-          <div className="item">
-            <div className="img-div">
-              <img
-                src="https://images.unsplash.com/photo-1514996937319-344454492b37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y29tcHVldCUyMGNsYXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt=""
-              />
-            </div>
-            <div className="info-div">
-              <h3>Lorem ipsum dolor sit amet.</h3>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                excepturi, qui perferendis laudantium architecto ea explicabo
-                nam ratione dolorem, aperiam possimus minus voluptates
-                exercitationem nihil aliquid temporibus. Dolor ex, consectetur
-                itaque cumque soluta est eos libero nulla totam ullam natus.
-              </p>
-              <span>8th January 2023 at 20:30 pm</span>
-            </div>
-          </div>
-        </div>
+        </form> */}
       </div>
     </div>
                 <Footer />
